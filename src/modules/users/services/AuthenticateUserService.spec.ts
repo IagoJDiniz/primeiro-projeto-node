550e1690034f6410807b122493d68dc8cab0e848
Iago Diniz <iagoo_ptu@hotmail.com>
2021-01-13 13:53:39 -0300

    Inserção de testes de autenticação com usuário inexistente e com password incorreto

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -31,6 +31,42 @@ describe('AuthenticateUser',()=>{
     expect(response.user).toEqual(user)
   })
 
+  it('should not be able to authenticate with non existing user',async ()=>{
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository,fakeHashProvider)
+    
+
+    expect(authenticateUser.execute({
+      email:'ahaaa@ecxx.com',
+      password:'123456'
+    })).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to authenticate with wrong password',async ()=>{
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository,fakeHashProvider)
+    const createUser = new CreateUserService(fakeUsersRepository,fakeHashProvider)
+
+    await createUser.execute({
+      name:"John Doe",
+      email:'ahaaa@ecxx.com',
+      password:'123456'
+    })
+
+
+
+    expect(authenticateUser.execute({
+      email:'ahaaa@ecxx.com',
+      password:'wrong-password'
+    })).rejects.toBeInstanceOf(AppError)
+  })
+
   
 
 })
